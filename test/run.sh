db98bbaec360d7f6b7efe741110a37ee90865b6a
DarekKasata <72450944+DarekKasata@users.noreply.github.com>
2021-09-30 22:15:47 +0530

    scripting the cortex code

--- test/run.sh
+++ test/run.sh
@@ -33,4 +33,27 @@ exit $ret
 fi
 rm -rf build
 
+
+
+
+
+
+/gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
+
+
+
+
+/gradlew compileJava
+ret=$?
+if [ $ret -ne 0 ]; then
+exit $ret
+fi
+rm -rf build
+
 exit
